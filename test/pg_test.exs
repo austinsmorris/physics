6bea33b49af85e1e37cd0b923ddbb327082c2e81
Austin Morris <austin.morris@gmail.com>
2015-12-24 09:56:33 -0500

    Generating a Better Solar Flare Dataset

--- test/pg_test.exs
+++ test/pg_test.exs
@@ -16,21 +16,20 @@ defmodule PGTest do
     {:ok, data: flares}
   end
 
-  test "Connecting with postgrex", %{data: flares} do
+  test "Querying with postgrex", %{data: flares} do
     {:ok, pid} = Postgrex.Connection.start_link(hostname: "localhost", database: "redfour")
 
     sql = """
-    insert into solar_flares(classification, scale, date)
-    values($1, $2, $3);
+    select * from solar_flares
     """
+    res = Postgrex.Connection.query!(pid, sql, []) |> transform_result
 
-    res = Enum.map flares, fn(flare) ->
-      ts = %Postgrex.Timestamp{year: flare.date.year, month: flare.date.month, day: flare.date.day}
-      Postgrex.Connection.query!(pid, sql, [Atom.to_string(flare.classification), flare.scale, ts])
-    end
-    
     IO.inspect res
     Postgrex.Connection.stop(pid)
   end
 
+  def transform_result(result) do
+    atomized = for col <- result.columns, do: String.to_atom(col)
+    for row <- result.rows, do: List.zip([atomized, row]) |> Enum.into(%{})
+  end
 end
